bd4b806cae7f0a631f7083af640ed7e6dfa3f8a6
hanniux <67535034+hanniux@users.noreply.github.com>
2022-01-14 12:36:34 +0630

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,80 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "ပြော",
+    artist: "Aung La",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "အရာရာဟာမင်းအတွက်",
+    artist: "Aung La",
+    img: "music-1",
+    src: "music-2"
+  },
+  {
+    name: "မင်းမရှိတဲ့ငါ",
+    artist: "Aung La",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "အချိန်လွန်ငှက်",
+    artist: "Aung La",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "အငှားအိမ်မက်",
+    artist: "Aung La",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "အပြစ် နဲ့ အမုန်း",
+    artist: "Aung La",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "ဘဝစစ်ရဲ့ မာတိကာ",
+    artist: "Aung La",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "My Home Paradise",
+    artist: "Aung La",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "အလွမ်းသင့်ပန်းချီ",
+    artist: "Aung La",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "God Send Rain",
+    artist: "Aung La",
+    img: "music-10",
+    src: "music-10"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
